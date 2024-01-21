13f1a1678d55ce47fa96dadbc0e89d16ccaf1165
Sarah <sarah.alewijnse@cefas.co.uk>
2024-01-21 15:36:13 +0000

    Rose plots for overviews

--- code/rose_code.R
+++ code/rose_code.R
@@ -16,7 +16,7 @@ hazard_scores <- read_xlsx(here::here("data",
 sector_cols <- c("#56B4E9",
                           "#009E73",
                           "#0072B2",
-                          "#F0E442")
+                          "#E69F00")
 
 names(sector_cols) <- c("Aquaculture",
                         "Crops",
@@ -25,19 +25,38 @@ names(sector_cols) <- c("Aquaculture",
 
 ## Category by sector ==========================================================
 
-ggplot(data = hazard_scores) +
+by_sector <- hazard_scores[, .(Score = sum(Score)),
+                           by = c("Category", "Sector")]
+ggplot(data = by_sector) +
   geom_hline(yintercept = seq(0, 400, 100), colour = "darkgrey") +
-  geom_bar(aes(x = Category, y = Score, fill = Sector), stat = "identity", alpha = 0.8) +
-  coord_polar() +
+  geom_bar(aes(x = Category, y = Score, fill = Sector, col = Sector), stat = "identity", alpha = 0.8,
+           width = 0.3) +
   scale_fill_manual(values = sector_cols) +
-  scale_y_continuous(limits = c(-10, 500), expand = c(0, 50)) +
+  scale_colour_manual(values = sector_cols) +
+  scale_y_continuous(limits = c(-5, 500), expand = c(0, 50)) +
   coord_polar() +
   theme_light() +
   ggtitle("Hazard categories by sector") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
+  theme(axis.ticks = element_blank(),
+        axis.text.y = element_blank(),
         axis.title = element_blank(),
         panel.grid = element_blank(),
+        legend.position = "bottom",
+        text = element_text(size = 15))
+
+ggplot(data = by_sector) +
+  geom_bar(aes(x = Category, y = Score, fill = Sector, col = Sector),
+           alpha = 0.8, stat = "identity", position = "dodge") +
+  scale_fill_manual(values = sector_cols) +
+  scale_colour_manual(values = sector_cols) +
+  scale_y_continuous(limits = c(-5, 150), expand = c(0, 50)) +
+  coord_polar() +
+  theme_light() +
+  ggtitle("Hazard categories by sector") +
+  theme(axis.ticks = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title = element_blank(),
+        panel.grid = element_line(colour = "grey"),
         legend.position = "bottom",
         text = element_text(size = 15))
 
